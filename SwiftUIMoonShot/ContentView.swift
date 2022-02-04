3388f014e517245eb28f06148703f5409b1d8ff6
Richard Price <richyprice77@gmail.com>
2022-02-04 12:42:36 +0000

    inital commit

---
+++ SwiftUIMoonShot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMoonShot
+//
+//  Created by Richard Price on 04/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIMoonShot/SwiftUIMoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMoonShotApp.swift
+//  SwiftUIMoonShot
+//
+//  Created by Richard Price on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
