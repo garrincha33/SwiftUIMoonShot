65534f10931fc05213ab66e05b845ebb3b6524c2
Richard Price <richyprice77@gmail.com>
2022-02-07 12:59:56 +0000

    added json files and a decoder to read the json

---
+++ SwiftUIMoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  SwiftUIMoonShot
+//
+//  Created by Richard Price on 07/02/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
